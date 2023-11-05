45ab3b5471fec4a26f586f67af155e230650fb3f
CyberKevHub <149927253+CyberKevHub@users.noreply.github.com>
2023-11-05 01:59:07 -0500

    Add files via upload

---
+++ VerySimpleCalculator.py
@@ -0,0 +1,24 @@
+"""
+Programmer: Kevin Flores
+Updated: 11/4/2023
+Purpose: Very Simple Calculator
+"""
+
+operator = input("Enter an operator (+ - * /): ")
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(round(result, 2 ))
+elif operator == "-":
+    result = num1 - num2
+    print(round(result, 2))
+elif operator == "*":
+    result = num1 * num2
+    print(round(result, 2))
+elif operator == "/":
+    result = num1 / num2
+    print(round(result, 2))
+else:
+    print(f"{operator} is not a valid operator")
